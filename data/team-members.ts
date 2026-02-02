4fabe72f9e115d4d7313c1edecbe024eabc2968a
Krishiv Thakuria <krishivthakuria@gmail.com>
2026-02-02 00:12:30 -0500

    opensource badge, team page edits

--- data/team-members.ts
+++ data/team-members.ts
@@ -3,21 +3,21 @@ import { TeamMember } from "@/types/team";
 export const teamMembers: TeamMember[] = [
     {
       name: 'Krishiv Thakuria',
-      role: 'President',
+      role: 'President & Chairman of the Board',
       image: '/profile-pics/krishiv-thakuria.png',
-      description: 'AI Engineer at MIT CSAIL (Kellis Lab); previously built AI tutoring software supported by Microsoft with 200,000+ users.',
+      description: 'Co-Creator and Instructor of MIT 6.S189 (Foundations and Frontiers of Generative AI) at MIT EECS; worked on AI at top Canadian startups.',
       linkedin: 'https://www.linkedin.com/in/krishiv-thakuria/'
     },
     {
       name: 'Hemit Patel',
       role: 'CTO',
       image: '/profile-pics/hemit-patel.png',
-      description: 'Software Engineer building for social impact; previously built a free SAT prep platform with 80,000+ users.',
+      description: 'Software Engineer across 3 industries building for social impact; previously developed a free SAT prep platform with 80,000+ users.',
       linkedin: 'https://www.linkedin.com/in/hemit-patel-383ab3271/'
     },
     {
       name: 'Danielle Dee',
-      role: 'COO',
+      role: 'COO and Board Director',
       image: '/profile-pics/danielle-dee.png',
       description: "Previously led a world champion robotics team, and ranked #1 out of 140,000+ competitors in the National Speech and Debate Association. ",
       linkedin: 'https://www.linkedin.com/in/danielle-d-a35b50262/'
